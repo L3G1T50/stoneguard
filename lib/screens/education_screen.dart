c188d829aefb3ccca399a3e9e7cece19be079008
Nick <djxl3g1t@gmail.com>
2026-05-06 16:30:56 -0400

    fix: replace all placeholder stubs with real screen implementations

--- lib/screens/education_screen.dart
+++ lib/screens/education_screen.dart
@@ -1,1 +1,13 @@
-EDUCATION_PLACEHOLDER
+import 'package:flutter/material.dart';
+import '../widgets/gradient_scaffold.dart';
+
+class EducationScreen extends StatelessWidget {
+  const EducationScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const GradientScaffold(
+      title: 'Education',
+      body: Center(child: Text('Education coming soon')),
+    );
+  }
+}
--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,1 +1,13 @@
-HISTORY_PLACEHOLDER
+import 'package:flutter/material.dart';
+import '../widgets/gradient_scaffold.dart';
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const GradientScaffold(
+      title: 'Stone History',
+      body: Center(child: Text('History coming soon')),
+    );
+  }
+}
